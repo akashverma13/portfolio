110a01056a72137dbd394caa5f611364b3cf489f
Akash Verma <itisverma@gmail.com>
2020-10-12 17:40:26 +0200

    Updates

---
+++ precache-manifest.2552fcf49210664701365feea8bf76bd.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76ed9ac92f760f18cb43664cc531849c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d7f91e06ffe7de1bdd82",
+    "url": "/portfolio/static/css/main.7a4ac3e2.chunk.css"
+  },
+  {
+    "revision": "41bf22792c3c39fea824",
+    "url": "/portfolio/static/js/2.deab6c11.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.deab6c11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7f91e06ffe7de1bdd82",
+    "url": "/portfolio/static/js/main.2b148f73.chunk.js"
+  },
+  {
+    "revision": "c53d9443373a56138b0b",
+    "url": "/portfolio/static/js/runtime-main.aad75174.js"
+  },
+  {
+    "revision": "6c16dd842c8a66076f05a0b7e145008d",
+    "url": "/portfolio/static/media/arrow-left.6c16dd84.svg"
+  },
+  {
+    "revision": "4d8a8472e198146ec19feaa01dc07ccd",
+    "url": "/portfolio/static/media/biglogo.4d8a8472.svg"
+  },
+  {
+    "revision": "68cfe602165938be738d71cebe8de172",
+    "url": "/portfolio/static/media/button.68cfe602.svg"
+  },
+  {
+    "revision": "bd0e47bf7fcf70d31521890e07a812b1",
+    "url": "/portfolio/static/media/circle.bd0e47bf.svg"
+  },
+  {
+    "revision": "bb6707fbf5fe8358cac1a0a46d6e38e9",
+    "url": "/portfolio/static/media/designsvector.bb6707fb.svg"
+  },
+  {
+    "revision": "6c1546daa3fa98fd8b752d74c84926db",
+    "url": "/portfolio/static/media/dletter.6c1546da.svg"
+  },
+  {
+    "revision": "1bb027109345a90a9eab1e929d8669c2",
+    "url": "/portfolio/static/media/github.1bb02710.svg"
+  },
+  {
+    "revision": "b9e3dd824c3a48d412923f68b95ab483",
+    "url": "/portfolio/static/media/homevector.b9e3dd82.svg"
+  },
+  {
+    "revision": "7ff1065e675570d876c3cc2351212101",
+    "url": "/portfolio/static/media/iletter.7ff1065e.svg"
+  },
+  {
+    "revision": "f5201de59360cbdc665f8eb32427116a",
+    "url": "/portfolio/static/media/infovector.f5201de5.svg"
+  },
+  {
+    "revision": "5b238434e2862c877f08572b96c0ef7b",
+    "url": "/portfolio/static/media/linkedin.5b238434.svg"
+  },
+  {
+    "revision": "b06c23cb5a28f7ac9cb57e3e3df8a20a",
+    "url": "/portfolio/static/media/pletter.b06c23cb.svg"
+  },
+  {
+    "revision": "349195fb8b7c5a6e5be35a29fa1835c7",
+    "url": "/portfolio/static/media/projectsvector.349195fb.svg"
+  },
+  {
+    "revision": "e540da0be72efc11d874d25b7b6d2462",
+    "url": "/portfolio/static/media/salehoo.e540da0b.svg"
+  },
+  {
+    "revision": "58fb2a1b6d6e7f093ed18f08c0a110a1",
+    "url": "/portfolio/static/media/skltn.58fb2a1b.svg"
+  },
+  {
+    "revision": "f48fc6aed85685ff98df4c5b9e5a8d3c",
+    "url": "/portfolio/static/media/small-logo.f48fc6ae.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/*/precache-manifest.a51e0c993603587243b6ecb491a47d88.js"
+  "/portfolio/precache-manifest.2552fcf49210664701365feea8bf76bd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/*/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
