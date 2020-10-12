4371baba75f411d7279ecc547b9013093b0fc06f
Akash Verma <itisverma@gmail.com>
2020-10-12 17:33:23 +0200

    Updates

---
+++ precache-manifest.a51e0c993603587243b6ecb491a47d88.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5574f0041e28db90c2e7b3446acb1ce4",
+    "url": "/*/index.html"
+  },
+  {
+    "revision": "87447dff157efe553196",
+    "url": "/*/static/css/main.508c5417.chunk.css"
+  },
+  {
+    "revision": "41bf22792c3c39fea824",
+    "url": "/*/static/js/2.deab6c11.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/*/static/js/2.deab6c11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87447dff157efe553196",
+    "url": "/*/static/js/main.f425b64f.chunk.js"
+  },
+  {
+    "revision": "91c541c7600e2e8e1244",
+    "url": "/*/static/js/runtime-main.459ff4c8.js"
+  },
+  {
+    "revision": "6c16dd842c8a66076f05a0b7e145008d",
+    "url": "/*/static/media/arrow-left.6c16dd84.svg"
+  },
+  {
+    "revision": "4d8a8472e198146ec19feaa01dc07ccd",
+    "url": "/*/static/media/biglogo.4d8a8472.svg"
+  },
+  {
+    "revision": "68cfe602165938be738d71cebe8de172",
+    "url": "/*/static/media/button.68cfe602.svg"
+  },
+  {
+    "revision": "bd0e47bf7fcf70d31521890e07a812b1",
+    "url": "/*/static/media/circle.bd0e47bf.svg"
+  },
+  {
+    "revision": "bb6707fbf5fe8358cac1a0a46d6e38e9",
+    "url": "/*/static/media/designsvector.bb6707fb.svg"
+  },
+  {
+    "revision": "6c1546daa3fa98fd8b752d74c84926db",
+    "url": "/*/static/media/dletter.6c1546da.svg"
+  },
+  {
+    "revision": "1bb027109345a90a9eab1e929d8669c2",
+    "url": "/*/static/media/github.1bb02710.svg"
+  },
+  {
+    "revision": "b9e3dd824c3a48d412923f68b95ab483",
+    "url": "/*/static/media/homevector.b9e3dd82.svg"
+  },
+  {
+    "revision": "7ff1065e675570d876c3cc2351212101",
+    "url": "/*/static/media/iletter.7ff1065e.svg"
+  },
+  {
+    "revision": "f5201de59360cbdc665f8eb32427116a",
+    "url": "/*/static/media/infovector.f5201de5.svg"
+  },
+  {
+    "revision": "5b238434e2862c877f08572b96c0ef7b",
+    "url": "/*/static/media/linkedin.5b238434.svg"
+  },
+  {
+    "revision": "b06c23cb5a28f7ac9cb57e3e3df8a20a",
+    "url": "/*/static/media/pletter.b06c23cb.svg"
+  },
+  {
+    "revision": "349195fb8b7c5a6e5be35a29fa1835c7",
+    "url": "/*/static/media/projectsvector.349195fb.svg"
+  },
+  {
+    "revision": "e540da0be72efc11d874d25b7b6d2462",
+    "url": "/*/static/media/salehoo.e540da0b.svg"
+  },
+  {
+    "revision": "58fb2a1b6d6e7f093ed18f08c0a110a1",
+    "url": "/*/static/media/skltn.58fb2a1b.svg"
+  },
+  {
+    "revision": "f48fc6aed85685ff98df4c5b9e5a8d3c",
+    "url": "/*/static/media/small-logo.f48fc6ae.svg"
+  }
+]);
